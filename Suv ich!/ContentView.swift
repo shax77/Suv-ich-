6d3f6752babc524c40ed2d7986b8a4e72e487c4c
shax77 <92374127+shax77@users.noreply.github.com>
2025-05-27 15:49:56 +0500

    Text qowildi va circle chizildi

--- Suv ich!/ContentView.swift
+++ Suv ich!/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fillAmount: CGFloat = 0
+    @State private var angle: Double = 0
+    @State private var percentage: Int = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Salom")
+            
+            Text("Suv Ichishni Eslatish")
+                .font(.system(size: 35,weight: .bold))
+            
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 15)
+                    .frame(width: 150,height: 150)
+                    .foregroundColor(.gray.opacity(0.3))
+                
+                Circle()
+                    .trim(from: 0,to: fillAmount)
+                    .stroke(style: StrokeStyle(lineWidth: 18, lineCap: .round, lineJoin: .round))
+                    .frame(width: 150,height: 150)
+                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.purple,.pink]), startPoint: .top, endPoint: .bottom))
+                
+                    }
+            
+            
         }
-        .padding()
+//        Spacer()
+//        
+//        .padding()
     }
 }
 
